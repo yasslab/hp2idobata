3b7dff0428e19c8175b76d6d46cda87a3f346ad3
Yohei YASUKAWA <yasulab@gmail.com>
2016-01-15 15:05:41 +0900

    Fit to Heroku Scheduler (Hourly)

--- main.rb
+++ main.rb
@@ -9,37 +9,49 @@ require 'csv'
 require 'pry'
 
 Idobata.hook_url = ENV['IDOBATA_END']
-CLIENT_ID     = ENV['CLIENT_ID']
-CLIENT_SECRET = ENV['CLIENT_SECRET'],
-REDIRECT_URI  = 'localhost'
-#AUTH_CODE = ENV['AUTH_CODE']
 ACCESS_TOKEN = ENV['ACCESS_TOKEN']
 
-DATE_TYPE    = 0
+# Required to generate ACCESS_TOKEN
+# See `sample_token_getter.rb` for details.
+#REDIRECT_URI  = 'localhost'
+#CLIENT_ID     = ENV['CLIENT_ID']
+#CLIENT_SECRET = ENV['CLIENT_SECRET'],
+#AUTH_CODE = ENV['AUTH_CODE']
+
+
 TAG          = 6021,6022,6023,6026,6027,6028
-TAG2NAME  = {
+TAG2NAME     = {
   '6021' => '体重', '6022' => '体脂肪率', '6023' => '筋肉量', '6024' => '筋肉スコア',
   '6025' => '内臓脂肪レベル', '6026' => '内臓脂肪レベル', '6027' => '基礎代謝量',
   '6028' => '体内年齢', '6029' => '推定骨量' }
-TAG2UNIT  = {
+TAG2UNIT     = {
   '6021' => 'kg', '6022' => '%', '6023' => 'kg', '6024' => '点',
   '6025' => 'レベル', '6026' => 'レベル', '6027' => 'kcal',
   '6028' => '歳', '6029' => 'kg' }
 TAG_PARAMS   = 6
-FROM_DATE0   = "20160114000000"
-TO_DATE0     = "20160114235900"
+DATE_TYPE    = 0
+FROM_DATE    = Time.now.strftime("%Y%m%d%H%M%S")
+TO_DATE      = (Time.now + 60 * 60).strftime("%Y%m%d%H%M%S") # in an hour
+#FROM_DATE    = "20160114000000"  # Sample Date Format
+#TO_DATE      = "20160114235900"  # Sample Date Format
 
 # Generate agent and get data via ACCESS_TOKEN
 agent = Mechanize.new
 agent.user_agent_alias = 'Windows IE 9'
 
-url = "https://www.healthplanet.jp/status/innerscan.xml?access_token=#{ACCESS_TOKEN}&date=#{DATE_TYPE}&tag=#{TAG}&from=#{FROM_DATE0}&to=#{TO_DATE0}"
+url = "https://www.healthplanet.jp/status/innerscan.xml?access_token=#{ACCESS_TOKEN}&date=#{DATE_TYPE}&tag=#{TAG}&from=#{FROM_DATE}&to=#{TO_DATE}"
 result  = agent.get(url)
 doc     = Nokogiri::HTML.parse(result.body.toutf8)
 data    = doc.xpath("//data")
 date    = doc.xpath("//date")
 keydata = doc.xpath("//keydata")
 tags    = doc.xpath("//tag")
+
+
+if data.empty?
+  puts "Nothing found in a recent hour."
+  exit
+end
 
 # Generate message to send Idobata
 msg = ""
